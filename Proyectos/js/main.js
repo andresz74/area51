9927b6833d55b1d5d61896c7643abff29d9729b2
Andres Zenteno <andres@Andress-MacBook-Pro.local>
2015-10-23 21:10:19 -0500

    Javascript para tab y menu

---
+++ Proyectos/js/main.js
@@ -0,0 +1,42 @@
+$( document ).ready(function() {
+    
+	$('.ui-tab').click(loadTab);
+
+	// var tabs = $('.ui-tab').toArray();
+	var tabs = $('.ui-tab');
+	var time = 0;
+
+	window.setInterval(reloadTab, 3000);
+
+	function reloadTab(){
+		if (time < tabs.length) {
+			if ( $(tabs[time]).hasClass('active') ) {
+				time++;
+			} else{
+				tabs[time].click(loadTab);
+				time++;	
+			};
+		} else{
+			time = 0
+		};
+	}
+
+	function loadTab(){
+		var activar = '#'+ $(this).data('tab');
+
+		$('.ui-tab').removeClass('active');
+		$('.tab-banner').hide().removeClass('active');
+
+		$(this).fadeIn(1000).addClass('active');
+		$(activar).fadeIn(1000).addClass('active');;
+	}
+
+	$('.min-menu').click(function(){
+		if (!$('.nav-menu').hasClass('active')) {
+			$('.nav-menu').addClass('active');
+		} else{
+			$('.nav-menu').removeClass('active');
+		};
+	});
+
+});
